e6d8647a0354b961bdbe194819ed954cd70c6174
sammosso <57271553+sammosso@users.noreply.github.com>
2020-02-25 14:03:47 -0800

    More learning examples added
    Learning examples I produced during the MSTA program.

---
+++ Back End Practice/C#/BubbleSort/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BubbleSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int[] inputArray = {8, 3, 9, 11, 2};
+
+            Console.WriteLine($"Add numbers with one space between each:");
+            //get user input
+            string inputValues = Console.ReadLine();
+            string[] inputStringArray = inputValues.Split(' ');
+            int[] inputArray = Array.ConvertAll(inputStringArray, int.Parse);
+            Console.WriteLine();
+
+            Console.Write($"My original array: ");
+            printArray(inputArray);
+            Console.WriteLine();
+            BubbleSort(inputArray);
+            ReverseBubbleSort(inputArray);
+        }
+        static void BubbleSort(int[] inArr)
+        {
+            int temp = 0;
+            for (int i = 1; i <= (inArr.Length - 1); i++)
+            {
+                for (int j = 0; j < (inArr.Length -1); j++)
+                {
+                    if (inArr[j + 1] < inArr[j])
+                    {
+                        temp = inArr[j];
+                        inArr[j] = inArr[j + 1];
+                        inArr[j + 1] = temp;
+                    }
+                }
+            }
+            Console.Write($"My sorted array: ");
+            printArray(inArr);
+            Console.WriteLine();
+        }
+        static void ReverseBubbleSort(int[] inArr)
+        {
+            int temp = 0;
+            for (int i = 1; i <= (inArr.Length - 1); i++)
+            {
+                for (int j = 0; j < (inArr.Length - 1); j++)
+                {
+                    if (inArr[j + 1] > inArr[j])
+                    {
+                        temp = inArr[j];
+                        inArr[j] = inArr[j + 1];
+                        inArr[j + 1] = temp;
+                    }
+                }
+            }
+            Console.Write($"My sorted array in reverse: ");
+            printArray(inArr);
+            Console.WriteLine();
+        }
+        static void printArray(int[] myArray)
+        {
+            foreach (var item in myArray)
+            {
+                Console.Write($"{item} ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Back End Practice/C#/Palindrome/palindrome/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace palindrome
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter some characters:");
+            string palindrome = Console.ReadLine();
+            checkPalidrome(palindrome);
+
+            if (checkPalidrome(palindrome))
+            {
+                Console.WriteLine("Yes! This is a Palidrome");
+            }
+            else
+            {
+                Console.WriteLine("No palindrome, try again!");
+            }
+            Console.WriteLine();
+
+        }
+
+        public static bool checkPalidrome(string palindrome)
+        {
+            char[] charArray = palindrome.ToCharArray();
+            Array.Reverse(charArray);
+            string newPalindrome = new string(charArray);
+
+            if (newPalindrome == palindrome)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
