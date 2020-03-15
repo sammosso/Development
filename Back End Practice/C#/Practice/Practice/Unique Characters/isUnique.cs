551463c3435bdad3a4e7d231ec5b747461f3d587
sammosso <57271553+sammosso@users.noreply.github.com>
2020-03-14 17:33:27 -0700

    Interview Practice problems

---
+++ Back End Practice/C#/Practice/Practice/Unique Characters/isUnique.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Practice
+{
+    class isUnique
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a word:");
+            string checkString = Console.ReadLine();
+            checkUnique(checkString);
+            Console.ReadLine();
+        }
+        private static void checkUnique(string s)
+        {
+            // loop through each character in string s.
+            for (int i = 1; i < s.Length; i++)
+            {
+                //check the second character against the previous character.
+                if (s[i - 1] == s[i])
+                {
+                    Console.WriteLine("String contains duplicate letters.");
+                    break;
+                }
+                // checks if the value of the index gets to the end.
+                else if (i == s.Length-1)
+                {
+                    Console.WriteLine("Every letter in this string is unique!");
+                }
+            }
+        }
+    }
+}
